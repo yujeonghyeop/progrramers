2e4d7c5f1d52f1bfd46ca622eab16ca62f805600
MIMO <one8775@ajou.ac.kr>
2021-09-17 17:18:36 +0900

    lotto_max,min_1

---
+++ Level1.lotto_max,min/lotto_max,min.py
@@ -0,0 +1,10 @@
+def solution(lottos, win_nums):
+    s=len((set(lottos) & set(win_nums)))
+    count1 = lottos.count(0)
+    if s==0 and count1==0:
+        answer = [6,6]
+    elif s==0:
+        answer = [7-(s+count1),6]
+    else:
+        answer = [7-(s+count1),7-s]
+    return answer
