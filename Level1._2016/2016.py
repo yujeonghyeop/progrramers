42b3fd23c31e37ff9190a6a752b0e0e9cd403d0c
MIMO <one8775@ajou.ac.kr>
2021-09-18 23:39:07 +0900

    ㅔ

--- Level1._2016/2016.py
+++ Level1._2016/2016.py
@@ -1,14 +1,7 @@
 def solution(a, b):
     dayname =["THU","FRI","SAT","SUN","MON","TUE","WED"]
     date =[0,31,29,31,30,31,30,31,31,30,31,30,31]
-    sum = 0
-    a=int(a)
-    b=int(b)
-    for i in range(0,a):
-        sum = date[i]+sum
-    answer = dayname[(sum+b)%7]
-    return answer
-
+    return dayname[(sum(date[:a])+b)%7]
 month1 = input()
 days1 = input()
 print(solution(month1,days1))
