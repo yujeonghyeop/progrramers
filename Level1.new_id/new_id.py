c262ac51a33ec3d80c222005f14b66b1c4b70921
MIMO <one8775@ajou.ac.kr>
2021-09-19 18:07:37 +0900

    programmers_3

--- Level1.new_id/new_id.py
+++ Level1.new_id/new_id.py
@@ -1,3 +1,16 @@
-def solution(new_id):
-    answer = ''
+import re
+def solution(new_id): #1단계
+    answer =''
+    second = re.sub(r'[^a-z0-9-._]',"",new_id.lower())
+    third = re.sub('[.]{2,}','.',second)
+    answer = third.strip("[.]")
+    if len(answer)==0:
+        answer ='aaa'
+    while len(answer)<3:
+            answer += answer[-1]
+    if len(answer)>=16:
+        answer=answer[:15]
+        answer = answer.strip("[.]")
     return answer
+a= input()
+print(solution(a))
