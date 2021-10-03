3b828a40153f24f16761ae1aa903121b983757a7
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-10-03 16:33:11 +0900

    programmers_32

--- Level2/Level2.openchat/openchat.py
+++ Level2/Level2.openchat/openchat.py
@@ -1,1 +1,15 @@
-w
+def solution(record):
+    answer = []
+    answer=[]
+    new1=" ".join(record)
+    new=new1.split()
+    inf={}
+    for i in range(len(new)):
+        if new[i]=='Enter' or new[i]=='Change':
+            inf[new[i+1]]=new[i+2]
+    for i in range(len(new)):
+        if new[i]=='Enter':
+            answer.append("{}님이 들어왔습니다.".format(inf[new[i+1]]))
+        elif new[i]=='Leave':
+            answer.append("{}님이 나갔습니다.".format(inf[new[i+1]]))
+    return answer
