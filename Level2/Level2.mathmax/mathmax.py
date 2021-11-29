64da0418a3576c97a14aa18e4f358f4bab7787ba
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2021-11-29 18:56:01 +0900

    programmers_75

---
+++ Level2/Level2.mathmax/mathmax.py
@@ -0,0 +1,38 @@
+import copy
+def solution(expression):
+    number=[["+","-","*"],["+","*","-"],["*","+","-"],["*","-","+"],["-","*","+"],["-","+","*"]]
+    a=[]
+    b=[]
+    cnt=0
+    c=[]
+    d=[]
+    k=0
+    max=0
+    for i in range(len(expression)):
+        if expression[i].isdigit() != True:
+            a.append(expression[i])
+            b.append(expression[cnt:i])
+            cnt=i+1
+    b.append(expression[cnt:])
+    for i in range(len(number)):  
+        c=copy.deepcopy(a)
+        d=copy.deepcopy(b)
+        for j in range(len(a)):
+            if number[i][0] in c:
+                k=c.index(number[i][0])
+                d[k]=str(eval(d[k]+number[i][0]+d[k+1]))
+                del c[k]
+                del d[k+1]
+            elif number[i][0] not in c and number[i][1] in c:
+                k=c.index(number[i][1])
+                d[k]=str(eval(d[k]+number[i][1]+d[k+1]))
+                del c[k]
+                del d[k+1]
+            elif number[i][0] not in c and number[i][1] not in c and number[i][2] in c:
+                k=c.index(number[i][2])
+                d[k]=str(eval(d[k]+number[i][2]+d[k+1]))
+                del c[k]
+                del d[k+1]
+        if abs(int(d[0]))>=max:
+            max= abs(int(d[0]))
+    return max
