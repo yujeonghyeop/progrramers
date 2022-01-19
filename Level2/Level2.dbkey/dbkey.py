dfe5f7823bafa61d65ff4326a373618904dd3a76
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-01-19 21:32:07 +0900

    programmers_89

---
+++ Level2/Level2.dbkey/dbkey.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+
+def solution(relation):
+    row,col = len(relation),len(relation[0])      
+    candidates = []
+    for i in range(1, col+1): 
+        candidates.extend(combinations(range(col), i))
+    unique = []
+    for keys in candidates:
+        tmp = [tuple([item[key] for key in keys]) for item in relation]
+        if len(set(tmp))==row:
+            unique.append(keys)
+    answer = set(unique)
+    for i in range(len(unique)):
+        for j in range(i+1, len(unique)):
+            if len(unique[i])==len(set(unique[i]).intersection(set(unique[j]))):
+                answer.discard(unique[j])  
+    return (len(answer))
