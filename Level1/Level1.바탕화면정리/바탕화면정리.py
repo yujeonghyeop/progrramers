f7b0f3117ad55f8a74ff31e8ba364380af929b49
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-07-06 10:04:36 +0900

    MIMO: 바탕화면 정리 풀이

--- Level1/Level1.바탕화면정리/바탕화면정리.py
+++ Level1/Level1.바탕화면정리/바탕화면정리.py
@@ -1,15 +1,14 @@
-wallpaper = ["..", "#."]	
-
-filex = []
-filey = []
-answer = []
-for i in range(len(wallpaper)):
-    for j in range(len(wallpaper[0])):
-        if wallpaper[i][j] == "#":
-            filex.append(i)
-            filey.append(j)
-answer.append(min(filex))
-answer.append(min(filey))
-answer.append(max(filex) + 1)
-answer.append(max(filey) + 1)
-print(answer)
+def solution(wallpaper):
+    filex = []
+    filey = []
+    answer = []
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == "#":
+                filex.append(i)
+                filey.append(j)
+    answer.append(min(filex))
+    answer.append(min(filey))
+    answer.append(max(filex) + 1)
+    answer.append(max(filey) + 1)
+    return answer
