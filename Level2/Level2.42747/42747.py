3355bc9d5361b99cb9afcb7d535b93b51770ed46
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-07-21 11:05:06 +0900

    7/21 풀이

---
+++ Level2/Level2.42747/42747.py
@@ -0,0 +1,13 @@
+def solution(citations):
+    citations = sorted(citations)
+    answer = min(citations)
+    for i in range(len(citations)+1):
+        for j in range(len(citations)-1):
+            if citations[j] <= i and citations[j+1] >= i:
+                upper = len(citations) - (j+1)
+                lower = len(citations) - upper
+                if upper >= i:
+                    answer = i
+    if answer > len(citations):
+        answer = len(citations)
+    return answer
